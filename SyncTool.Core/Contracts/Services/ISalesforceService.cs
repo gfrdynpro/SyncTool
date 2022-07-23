d57bf555cb01f5ac1c7c3136d9112f98bca75432
Gerry <grohling@dynpro.com>
2022-07-23 09:08:24 +0100

    New icon and blank service clients

---
+++ SyncTool.Core/Contracts/Services/ISalesforceService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyncTool.Core.Contracts.Services;
+internal interface ISalesforceService
+{
+}
---
+++ SyncTool.Core/Services/SalesforceService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SyncTool.Core.Contracts.Services;
+
+namespace SyncTool.Core.Services;
+public class SalesforceService : ISalesforceService
+{
+}
